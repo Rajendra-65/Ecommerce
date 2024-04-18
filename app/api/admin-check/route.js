fc3fd20a0df8acca8cb543ecc729793ab462a81d
Rajendra Behera <145452539+Rajendra-65@users.noreply.github.com>
2024-04-18 11:14:17 +0530

    Update route.js

--- app/api/admin-check/route.js
+++ app/api/admin-check/route.js
@@ -5,23 +5,21 @@ import { connectDb } from "../../../utils/connectdb"
 export const GET = async () => {
     try{
         await connectDb()
-        let flag = null
+        let flag = 0
         const user = await currentUser()
         const userEmail = user.emailAddresses[0].emailAddress
-        console.log(userEmail)
         for(let i=0;i<adminEmails.length;i++){
             if(userEmail === adminEmails[i]){
                 flag = 1
                 break
             }
         }
-        console.log(flag)
         if(flag){
             return NextResponse.json({success:true,message:"The user is a admin User",data:true})
         }else{
             return NextResponse.json({success:false,message:"The user is not an admin User",data:false})
         }
     }catch(e){
-        return NextResponse.json({success:false,message:"Error in the try block"})
+        return NextResponse.json({success:false,message:"Error in the try block",error:e})
     }
 }
