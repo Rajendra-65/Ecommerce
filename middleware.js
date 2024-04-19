82c0bdb1e782c9969d482003c8a55099b4243b11
Rajendra Behera <145452539+Rajendra-65@users.noreply.github.com>
2024-04-19 11:54:28 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ import { usePathname } from "next/navigation";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes:["/","all-products"],
+  publicRoutes:["/","/all-products"],
   afterAuth(auth, req, evt) {
     // handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
