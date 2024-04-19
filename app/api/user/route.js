5eca47b5c21fab9e7adb066ef005ecf680eb6a59
Rajendra Behera <145452539+Rajendra-65@users.noreply.github.com>
2024-04-19 19:55:15 +0530

    Update route.js

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -2,6 +2,7 @@ import { currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import {User} from "../../../models/User"
 import {connectDb} from "../../../utils/connectdb"
+import { adminEmails } from "../../../utils";
 
 export const  GET = async () => {
     try{
@@ -10,8 +11,11 @@ export const  GET = async () => {
         const user = await currentUser()
         const userEmail = user.emailAddresses[0].emailAddress
         const userDetails = await User.findOne({email:userEmail})
-        if(userDetils.admin){
-            admin = true
+        for(let i=0;i<adminEmails.length;i++){
+            if(userDetails.email === adminEmails[i]){
+                admin = true
+                break
+            }
         }
         return NextResponse.json({status:200,statusText:"successfully Posted to the client",data:userDetails,admin:admin})
     }catch(error){
