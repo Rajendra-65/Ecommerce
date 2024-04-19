baf4cdb012aee7c3c3ef440ecec672fa712cafd1
Rajendra Behera <145452539+Rajendra-65@users.noreply.github.com>
2024-04-19 16:43:52 +0530

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
       if (userDetails) {
         setUser(true);
       }
-      if(userDetails.data.admin){
+      if(userDetails.admin){
         setIsAdminView(true)
       }
     };
