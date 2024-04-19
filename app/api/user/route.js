09db3a7a1c8155d30dcba2fa2226a3b4c1bd2418
Rajendra Behera <145452539+Rajendra-65@users.noreply.github.com>
2024-04-19 16:45:36 +0530

    Update route.js

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -6,10 +6,14 @@ import {connectDb} from "../../../utils/connectdb"
 export const  GET = async () => {
     try{
         await connectDb()
+        let admin = false
         const user = await currentUser()
         const userEmail = user.emailAddresses[0].emailAddress
         const userDetails = await User.findOne({email:userEmail})
-        return NextResponse.json({status:200,statusText:"successfully Posted to the client",data:userDetails})
+        if(userDetils.admin){
+            admin = true
+        }
+        return NextResponse.json({status:200,statusText:"successfully Posted to the client",data:userDetails,admin:admin})
     }catch(error){
         console.log(error)
         return NextResponse.json({status:"NOT Ok",message:"Error in sending to the client page"})
