a8a11a7ed2401a1abd187a2dbfc485bf2f9017ea
Rajendra Behera <145452539+Rajendra-65@users.noreply.github.com>
2024-04-20 09:57:58 +0530

    Update connectdb.js

--- utils/connectdb.js
+++ utils/connectdb.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose"
+import { CreateUser } from "./userCreation";
 export const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_URL);
+        await Createuser()
     } catch (error) {
         console.error("Connection Failed:", error);
     }
